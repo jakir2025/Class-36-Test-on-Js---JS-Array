770b9c0e9b1a2b80b39960e51146d0ec33e4e2ef
jakir9324 <jakirhusen9324@gmail.com>
2025-02-12 11:16:50 +0600

    text

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+let ticketPrice;
+
+age = 4;
+
+if(age >= 60){
+    console.log("the ticket price $8");
+}
+else if(age >=18 && age <= 59 ){
+    console.log("the ticket price $15");
+}
+else if(age >= 5 && age <= 17){
+    console.log("the ticket price $10");
+}
+else{
+    console.log("the ticket price is free")
+}
